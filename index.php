396789a1170ccc21a156bc7eee84012c9019a678
Qleroty <quentinleroty@gmail.com>
2017-12-21 14:57:29 +0100

    dz

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$user = new Utilisateur();
+
+class Utilisateur
+{
+  private $nom = "Doe";
+  private $prenom = "John";
+  private $email ="john.doe@domain.com";
+  private $password ="mdpsupersecure";
+
+}
+
+ ?>
+
+<form class="" action="index.html" method="post">
+  <legend>
+<label for=""></label>
+<input type="text" name="" value="" placeholder="John">
+<input type="text" name="" value="" placeholder="Doe">
+<input type="email" name="" value="" placeholder="john.doe@domain.com">
+<input type="password" name="" value="" placeholder="mdpsupersecure">
+  </legend>
+</form>
