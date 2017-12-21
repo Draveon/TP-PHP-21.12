f527363d848698ee78a34f7d0c4d6906882c2ac4
Qleroty <quentinleroty@gmail.com>
2017-12-21 15:10:31 +0100

    ded

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
+session_start();
 
 $user = new Utilisateur();
 
 class Utilisateur
 {
-  private $nom = "Doe";
-  private $prenom = "John";
-  private $email ="john.doe@domain.com";
-  private $password ="mdpsupersecure";
+$_SESSION['prenom'] = 'John';
+$_SESSION['nom'] = 'Doe';
+$_SESSION['email'] = 'john.doe@domain.com';
+$_SESSION['password'] = 'mdpsupersecure';
+
+
 
 }
 
  ?>
-
+<h1>Accueil du site : Login</h1>
 <form class="" action="index.html" method="post">
   <legend>
-<label for=""></label>
+<label for="">Prenom</label>
 <input type="text" name="" value="" placeholder="John">
+<label for="">Nom</label>
 <input type="text" name="" value="" placeholder="Doe">
+<label for="">Email</label>
 <input type="email" name="" value="" placeholder="john.doe@domain.com">
+<label for="">Password</label>
 <input type="password" name="" value="" placeholder="mdpsupersecure">
   </legend>
 </form>
